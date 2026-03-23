fdf4dffd171ee87fd35583b5ce19a0143b6fff44
Stephan Pirnbaum <Stephan.Pirnbaum@googlemail.com>
2026-03-23 20:03:53 +0100

    Fix resolve of DSL includes inside jQAssistant Plugin Jars

--- src/main/java/de/materna/structurizr/jqassistant/plugin/StructurizrWorkspaceScannerPlugin.java
+++ src/main/java/de/materna/structurizr/jqassistant/plugin/StructurizrWorkspaceScannerPlugin.java
@@ -16,8 +16,14 @@ import de.materna.structurizr.jqassistant.plugin.persister.WorkspacePersister;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * Scanner plug-in to enrich the graph based on a Structurizr DSL file.
@@ -30,30 +36,16 @@ public class StructurizrWorkspaceScannerPlugin extends AbstractScannerPlugin<Fil
 
     @Override
     public boolean accepts(FileResource fileResource, String path, Scope scope) {
-        if (!path.toLowerCase().endsWith(".dsl")) {
-            return false;
-        } else {
-            try (InputStream is = fileResource.createStream()) {
-                String fileContent = IOUtils.toString(is);
-                StructurizrDslParser parser = new StructurizrDslParser();
-                parser.parse(fileContent, fileResource.getFile());
-                parser.getWorkspace();
-                return true;
-            } catch (Exception e) {
-                log.error("Unable to read Structurizr workspace", e);
-                return false;
-            }
-        }
+        return path.toLowerCase().endsWith(".dsl");
     }
 
     @Override
     public StructurizrDescriptor scan(FileResource fileResource, String path, Scope scope, Scanner scanner) throws IOException {
         StructurizrFileDescriptor structurizrFileDescriptor = getScannerContext().getStore().addDescriptorType(getScannerContext().getCurrentDescriptor(), StructurizrFileDescriptor.class);
-
         try (InputStream is = fileResource.createStream()) {
             String fileContent = IOUtils.toString(is);
             StructurizrDslParser parser = new StructurizrDslParser();
-            parser.parse(fileContent, fileResource.getFile());
+            parser.parse(fileContent, resolveWorkspace(fileResource, path));
             Workspace workspace = parser.getWorkspace();
             WorkspacePersister persister = new WorkspacePersister(getScannerContext().getStore());
             WorkspaceDescriptor workspaceDescriptor = persister.persist(workspace);
@@ -61,6 +53,52 @@ public class StructurizrWorkspaceScannerPlugin extends AbstractScannerPlugin<Fil
             return structurizrFileDescriptor;
         } catch (StructurizrDslParserException e) {
             throw new StructurizrWorkspaceEvaluationException("Unable to parse Structurizr workspace", e);
+        } catch (URISyntaxException e) {
+            throw new StructurizrWorkspaceEvaluationException("Unable to resolve workspace files in jQAssistant Plugin Jar", e);
+        }
+    }
+
+    private File resolveWorkspace(FileResource fileResource, String path) throws URISyntaxException, IOException {
+        if ("jar".equals(new URI(path).getScheme())) {
+            /*
+             * Workspace is part of a jQAssistant Plug-In
+             * Includes can't be resolved by Structurizr as they are in the jar file
+             * so extract all the DSLs into a temp folder
+             */
+            Path tempDir = Files.createTempDirectory("structurizr-dsl-");
+            URI jarURI = resolveJarURI(new URI(path));
+
+            try (JarFile jar = new JarFile(Path.of(jarURI).toFile())) {
+                jar
+                        .stream()
+                        .filter(f -> f.getName().endsWith(".dsl") || f.getName().endsWith(".json")) // included DSLs and Themes
+                        .forEach(entry -> extract(jar, entry, tempDir));
+            }
+            return null;
+        } else {
+            return fileResource.getFile();
+        }
+    }
+
+    private URI resolveJarURI(URI jarURI) {
+        // split jar:file:/...jar!/<entry>
+        String ssp = jarURI.getSchemeSpecificPart(); // file:/...jar!/workspace.dsl
+        int bang = ssp.indexOf("!/");
+        if (bang < 0) throw new IllegalArgumentException("Invalid jar URI: " + jarURI);
+
+        return URI.create(ssp.substring(0, bang));      // /.../artifact.jar
+    }
+
+    private void extract(JarFile jar, JarEntry entry, Path targetDir) {
+        try {
+            Path target = targetDir.resolve(entry.getName());
+
+            Files.createDirectories(target.getParent());
+            try (InputStream in = jar.getInputStream(entry)) {
+                Files.copy(in, target, StandardCopyOption.REPLACE_EXISTING);
+            }
+        } catch (IOException ex) {
+            throw new RuntimeException("Fehler beim Entpacken von " + entry.getName(), ex);
         }
     }
 }
